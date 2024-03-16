90d71181e1591601cc2b7c0439a53daab823178a
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2024-03-16 17:54:36 +0530

    hosting error resolved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import "./App.css";
+import "./App.css";
 
 export default function App() {
   const [result, setResult] = useState();
@@ -40,7 +40,7 @@ export default function App() {
   };
 
   return (
-    <div className="appBlock">
+    <div className="App">
       <form onSubmit={handleSubmit} className="form">
         <label className="questionLabel" htmlFor="question">
           Question:
