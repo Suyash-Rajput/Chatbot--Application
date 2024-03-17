f40fb4311116bdde7e8ede20057e575ba8f6071a
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2024-03-17 19:52:37 +0530

    host

--- backend/result.py
+++ backend/result.py
@@ -10,8 +10,6 @@ from langchain.callbacks import get_openai_callback
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
 
-
-
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 genai.configure(api_key=API_KEY)
@@ -25,29 +23,26 @@ prompt_template = """Answer the question as precise as possible using the provid
 def get_text_chunks(text):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=10000, chunk_overlap=1000)
-    print("text ------", type(text))
     chunks = splitter.split_text(str(text))
     return chunks  # list of strings
 
-def  get_result(pages, query):
-#     text_splitter = RecursiveCharacterTextSplitter(
-#             chunk_size=1000,
-#             chunk_overlap=200,
-#             )
-    chunks = get_text_chunks(pages)
-#     page_content = "\n".join(str(p.page_content) for p in pages)
-#     chunks = text_splitter.split_text(page_content)
-    print("chunks ----------", chunks)
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001", google_api_key =  API_KEY)   
-    VectorStore = Chroma.from_texts(chunks, embeddings).as_retriever()
-    if query:
+def get_result(pages, query):
+        chunks = get_text_chunks(pages)
+        print("chunks ----------", chunks)
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=API_KEY)
+        VectorStore = Chroma.from_texts(chunks, embeddings).as_retriever()
+        if query:
             print(" query --- ", query)
             docs = VectorStore.get_relevant_documents(query)
             print("Docs:-     --------------  ", docs)
             llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True, google_api_key=API_KEY)
-            prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-            chain = load_qa_chain(llm =llm, chain_type="stuff", prompt=prompt)
-            response = chain({"input_documents": docs, "question": query}, return_only_outputs=True)
+            prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+            chain = load_qa_chain(llm=llm, chain_type="stuff", prompt=prompt)
+            try:
+              response = chain({"input_documents": docs, "question": query}, return_only_outputs=True)
+            except Exception as e:
+              print(f"An error occurred: {e}")
+              return "LLM API is in trouble. Please try again."
             print(response)
             return response
-    return "None"
+        return "None"
