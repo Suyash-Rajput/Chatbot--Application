55e1314bddd8714debfbf55b1486a8b50e5b24a1
Suyash-Rajput <66985650+Suyash-Rajput@users.noreply.github.com>
2024-03-30 13:25:26 +0530

    s

--- backend/main.py
+++ backend/main.py
@@ -75,11 +75,12 @@ def get_text(fileType, file):
 
 @app.post("/predict")
 async def predict(request: Request, file: UploadFile = File(...), question: str = Form(...), fileType: str = Form(...)):
-    # print("fileType --------", fileType)
-    # pages = get_text(fileType, file)
-    # print("raw_text --------", pages)
-    # resp = result_class.get_result(pages, question)
-    # print("Question:", question)
-    # return {"result": resp['output_text']}
-    return {"result" : "suyash is working"}
-    # return {"result": [pages, question]}
+    print("fileType --------", fileType)
+    pages = get_text(fileType, file)
+    print("raw_text --------", pages)
+    resp = result_class.get_result(pages, question)
+    print("Question:", question)
+    return {"result": resp['output_text']}
+    # return {"result" : "suyash is working"}
+    # return {"result": [pages, question]}    
+    
